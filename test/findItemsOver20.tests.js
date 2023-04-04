0fc2f4d0ed24f71c105db88425dd29bce4597332
Yamisa <majijayamisa@gmail.com>
2023-04-04 16:14:09 +0200

    update

--- test/findItemsOver20.tests.js
+++ test/findItemsOver20.tests.js
@@ -1,14 +1,33 @@
-let fruitsEaten = [
-    {name : 'apples', qty : 10},
-    {name : 'pears', qty : 37},
-    {name : 'bananas', qty : 27},
-    {name : 'apples', qty : 3},
-];
+
 
 describe('Test my findItemsOver20 function' , function(){
     it('should return an array with items that have quantity bigger than 20' , function(){
+        let fruitsEaten = [
+            {name : 'apples', qty : 10},
+            {name : 'pears', qty : 37},
+            {name : 'bananas', qty : 27},
+            {name : 'apples', qty : 3},
+        ];
         assert.deepEqual([{ name: 'pears', qty: 37 }, { name: 'bananas', qty: 27 }],findItemsOver20(fruitsEaten));
 
     });
-    
+    it('should return 4 items in array with qty bigger than 20' , function(){
+        let fruitsEaten2 = [
+            {name : 'apples', qty : 30},
+            {name : 'pears', qty : 37},
+            {name : 'bananas', qty : 27},
+            {name : 'apples', qty : 21},
+        ];
+        assert.deepEqual([{name : 'apples', qty : 30},
+        {name : 'pears', qty : 37},
+        {name : 'bananas', qty : 27},
+        {name : 'apples', qty : 21}],findItemsOver20(fruitsEaten2));
+
+    });
+    it('should return an empty array. no items in array' , function(){
+        let fruitsEaten3 = [
+        ];
+        assert.deepEqual([],findItemsOver20(fruitsEaten3));
+
+    });
 });
